bbf5c85a2780b770cbcb2ff31678210f57f76a17
nyx-a <nyx-a@live.jp>
2020-12-06 16:17:49 +0900

    iroiro

---
+++ el/suehiro.el
@@ -0,0 +1,11 @@
+
+(setq my-fontsize 12)
+(setq my-asciifont "monospace")
+(setq my-jpfont "Osaka")
+(setq my-window-alpha '(100 100))
+
+(setq my-newline-mark '(?\n [?\x21A9 ?\n]))
+
+(setq my-frame-height 55)
+(setq my-frame-width 80)
+
--- el/wrid.el
+++ el/wrid.el
@@ -1,6 +1,6 @@
 
 (setq wrid-directory     "~")
-(setq wrid-file-format   "%Y-%m-%d-%a.diary.txt")
+(setq wrid-file-suffix   ".diary.txt")
 (setq wrid-header-format "%Y %-m-%-d %A\n\n")
 (setq wrid-go-back-hours 4)
 
@@ -11,13 +11,35 @@
    (+ (* wrid-go-back-hours 60 60)
       (* (if (equal backday nil) 0 backday) 24 60 60))))
 
+(defun wrid-time-y (time)
+  (format-time-string "%Y" time))
+
+(defun wrid-time-m (time)
+  (format-time-string "%m" time))
+
+(defun wrid-time-d (time)
+  (format-time-string "%d" time))
+
+(defun wrid-time-w (time)
+  (nth (string-to-number
+        (format-time-string "%w" time))
+       '("sun" "mon" "tue" "wed" "thu" "fri" "sat")))
+
+(defun wrid-filename (time)
+  (concat (string-join
+           (list (wrid-time-y time)
+                 (wrid-time-m time)
+                 (wrid-time-d time)
+                 (wrid-time-w time))
+           "-")
+          wrid-file-suffix))
+
 (defun wrid-path (&optional time)
-  (if (equal time nil) (setq time (wrid-current-time)))
-  (format
-   "%s/%s/%s"
-   wrid-directory
-   (format-time-string "%Y" time)
-   (downcase (format-time-string wrid-file-format time))))
+  (let ((time (or time (wrid-current-time))))
+    (format "%s/%s/%s"
+            wrid-directory
+            (wrid-time-y time)
+            (wrid-filename time))))
 
 (defun wrid-insert-header (&optional time)
   (interactive "P")
@@ -40,7 +62,6 @@
   "WRIte Diary / C-u Days back"
   (interactive "P")
   (wrid-find-file (wrid-current-time backday)))
-
 
 (defun wrid-ymd (text)
   "WRIte Diary at specified date"
